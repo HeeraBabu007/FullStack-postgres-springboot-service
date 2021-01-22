77266fd32d22a0e0789fdc49e25263710d0d42ae
Heera Babu <heerababu80@gmail.com>
2021-01-22 23:25:08 +0530

    added CrossOrigin
    added CrossOrigin

--- src/main/java/com/ge/preparedbyheera/controller/DepartmentController.java
+++ src/main/java/com/ge/preparedbyheera/controller/DepartmentController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ge.preparedbyheera.model.Department;
 import com.ge.preparedbyheera.repository.DepartmentRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class DepartmentController {
 
